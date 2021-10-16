0be5944e3ed31d91cffb86d4352561a76868b0e6
toryua <87789457+toryua@users.noreply.github.com>
2021-10-16 14:12:46 -0700

    added app.POST functionality to push objects to the array and write the file

--- server.js
+++ server.js
@@ -1,4 +1,48 @@
 const PORT = process.env.PORT || 3001;
+const express = require('express');
+const app = express();
+var uniqid = require('uniqid');
+const data = require('./db/db.json');
+const fs = require('fs');
+
+// bring in midddleware
+app.use(express.json());
+app.use(express.urlencoded(true));
+app.use(express.static('public'));
+
+// read what's in db.json and write it to the front end
+// pull data into a route
+
+
+
+app.get('/notes', function (req, res) {
+  res.sendFile(__dirname + '/public/notes.html')
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/index.html')
+});
+
+app.get('/api/notes', function (req, res) {
+  res.json(data)
+});
+
+app.post('/api/notes', function (req, res) {
+  console.log(req.body.text + " line 31");
+  //req.body.title
+  const note = data
+  data.push({ 'title': req.body.title, 'text': req.body.text });
+  
+  fs.appendFile('./db/db.json', JSON.stringify(note), (err)=> {
+    if (err) console.log('did not work')
+  });
+  
+  console.log(req.body)
+});
+
+app.get('/api/notes', function(req, res) {
+  res.data
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
