f40dc0a74ed3761a30f54a2ba091bea920b2d7bd
toryua <87789457+toryua@users.noreply.github.com>
2021-10-18 18:40:48 -0700

    checking commit to make sure everything is up to date so heroku will deploy

--- server.js
+++ server.js
@@ -1,20 +1,15 @@
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 3001;
 const express = require('express');
 const app = express();
 var uniqid = require('uniqid');
 const data = require('./db/db.json');
 const fs = require('fs');
-const router = express.Router();
+//const router = express.Router();
 
 // bring in midddleware
 app.use(express.json());
 app.use(express.urlencoded(true));
 app.use(express.static('public'));
-
-// read what's in db.json and write it to the front end
-// pull data into a route
-
-
 
 app.get('/notes', function (req, res) {
   res.sendFile(__dirname + '/public/notes.html')
@@ -42,9 +37,6 @@ app.post('/api/notes', function (req, res) {
   console.log(req.body);
   
 });
-
-
-
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
